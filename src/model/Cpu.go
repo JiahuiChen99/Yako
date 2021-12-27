38cf7bc2225c54f7c55d34cc7a6aa0f042f3c7c2
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-27 20:50:01 +0100

    #3 feat: cpu model is json serializable

--- src/model/Cpu.go
+++ src/model/Cpu.go
@@ -1,7 +1,9 @@
 package model
 
+// Cpu
+//Representation of a processor modeled after /proc/cpuinfo
 type Cpu struct {
-	Id     uint
-	Core   uint
-	Socket uint
+	Id     uint `json:"id"`
+	Core   uint `json:"core"`
+	Socket uint `json:"socket"`
 }

d2258135a9c51fe5034bb4295dbb70a390a43a37
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-27 21:54:56 +0100

    #9 feat: cpu get resources - open cpuinfo file - closes file

--- src/model/Cpu.go
+++ src/model/Cpu.go
@@ -1,9 +1,24 @@
 package model
 
+import "os"
+
 // Cpu
 //Representation of a processor modeled after /proc/cpuinfo
 type Cpu struct {
 	Id     uint `json:"id"`
 	Core   uint `json:"core"`
 	Socket uint `json:"socket"`
+}
+
+// GetResources Retrieves information related to the system cpu
+// currently only Linux is supported
+func (c *Cpu) GetResources() {
+	// Open the file
+	f, err := os.Open("/proc/cpuinfo")
+	if err != nil {
+		panic("Failed to open /proc/cpuinfo")
+	}
+	// Deferred file close
+	defer f.Close()
+
 }

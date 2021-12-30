14f331f172278d1832942a0921da9638f2251e51
JiahuiChen99 <Jiahui1@hotmail.es>
2021-12-30 14:27:03 +0100

    #11 feat: get gpu directories names

--- src/model/Gpu.go
+++ src/model/Gpu.go
@@ -1,8 +1,38 @@
 package model
 
+import (
+	"os"
+)
+
 // Gpu
 // Abstraction of a GPU
 type Gpu struct {
 	Major uint `json:"major"`
 	Minor uint `json:"minor"`
+}
+
+func (g Gpu) GetResources() interface{} {
+	// Check if Nvidia drivers are installed
+	if _, err := os.Stat("/proc/driver/nvidia"); os.IsNotExist(err) {
+		panic("Please install nvidia drivers")
+	}
+
+	// Check for GPU information
+	gpusDir, err := os.Open("/proc/driver/nvidia/gpus")
+	if err != nil {
+		panic("Error while parsing GPUs directory " + err.Error())
+	}
+
+	gpusDirFiles, err := gpusDir.Readdir(-1)
+	if err != nil {
+		panic("Error while parsing GPUs directory " + err.Error())
+	}
+	gpusDir.Close()
+
+	// Get GPU directories names (multi-gpu support)
+	for _, file := range gpusDirFiles {
+
+	}
+
+	return nil
 }

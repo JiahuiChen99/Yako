14d69444461666b79e27ef7b857fae9fef17adc9
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-01 21:39:19 +0100

    #13 feat: open /proc/meminfo file

--- src/model/Memory.go
+++ src/model/Memory.go
@@ -1,5 +1,7 @@
 package model
 
+import "os"
+
 // Memory
 // Representation of the main memory, the unit is in kB
 type Memory struct {
@@ -7,4 +9,14 @@ type Memory struct {
 	Free      int `json:"free"`     // "MemFree" system unused memory
 	TotalSwap int `json:"swap"`     // "SwapTotal"
 	FreeSwap  int `json:"freeSwap"` // "SwapFree"
+}
+
+func (m Memory) GetResources() interface{} {
+	// Open the file
+	f, err := os.Open("/proc/meminfo")
+	if err != nil {
+		panic("Failed to open /proc/meminfo")
+	}
+	defer f.Close()
+
 }

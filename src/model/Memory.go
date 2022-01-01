502d9183b225f791a28de29173826c060db0b6b7
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-01 22:04:05 +0100

    #13 feat: parse meminfo and retrieve main & swap memory information

--- src/model/Memory.go
+++ src/model/Memory.go
@@ -1,6 +1,12 @@
 package model
 
-import "os"
+import (
+	"bufio"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 // Memory
 // Representation of the main memory, the unit is in kB
@@ -23,4 +29,32 @@ func (m Memory) GetResources() interface{} {
 	}
 	defer f.Close()
 
+	scanner := bufio.NewScanner(f)
+	var memory Memory
+
+	// Scan the file line by line
+	for scanner.Scan() {
+		// Split each row and get the column name
+		if scannerLine := twoColRegexMemory.Split(scanner.Text(), 2); scannerLine != nil {
+			// Remove the memory units
+			noUnits := strings.Split(scannerLine[1], " ")[0]
+			memoryQuantity, err := strconv.Atoi(noUnits)
+			if err != nil {
+				panic("Couldn't parse " + scannerLine[0] + "with" + scannerLine[1] + "format.")
+			}
+
+			switch scannerLine[0] {
+			case "MemTotal":
+				memory.Total = memoryQuantity
+			case "MemFree":
+				memory.Free = memoryQuantity
+			case "SwapTotal":
+				memory.TotalSwap = memoryQuantity
+			case "SwapFree":
+				memory.FreeSwap = memoryQuantity
+			}
+		}
+	}
+
+	return memory
 }

c99f86cdafd97e608f84f6eaa319d7f28e88d9a7
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-11 22:22:38 +0000

    #23 feat: yako node system service grpc services implementation

---
+++ src/yako_node/services/SystemService.go
@@ -0,0 +1,26 @@
+package yako_nodeservices
+
+import (
+	"context"
+	"github.com/golang/protobuf/ptypes/empty"
+	"yako/src/grpc/yako"
+)
+
+// YakoNodeServer implements NodeServiceServer interface
+type YakoNodeServer struct {
+}
+
+func (ns *YakoNodeServer) GetSystemInformation(ctx context.Context, empty *empty.Empty) (*yako.SysInfo, error) {
+}
+
+func (ns *YakoNodeServer) GetSystemCpuInformation(ctx context.Context, empty *empty.Empty) (*yako.Cpu, error) {
+}
+
+func (ns *YakoNodeServer) GetSystemGpuInformation(ctx context.Context, empty *empty.Empty) (*yako.Gpu, error) {
+}
+
+func (ns *YakoNodeServer) GetSystemMemoryInformation(ctx context.Context, empty *empty.Empty) (*yako.Memory, error) {
+}
+
+func (ns *YakoNodeServer) mustEmbedUnimplementedNodeServiceServer() {
+}

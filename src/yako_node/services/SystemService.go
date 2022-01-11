6b3844d17e449b742cd4a57e3c8e0510e3d9c91b
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-11 23:08:07 +0000

    #23 refactor: get cpu & gpu information now returns a cpu & gpu list

--- src/yako_node/services/SystemService.go
+++ src/yako_node/services/SystemService.go
@@ -13,10 +13,10 @@ type YakoNodeServer struct {
 func (ns *YakoNodeServer) GetSystemInformation(ctx context.Context, empty *empty.Empty) (*yako.SysInfo, error) {
 }
 
-func (ns *YakoNodeServer) GetSystemCpuInformation(ctx context.Context, empty *empty.Empty) (*yako.Cpu, error) {
+func (ns *YakoNodeServer) GetSystemCpuInformation(ctx context.Context, empty *empty.Empty) (*yako.CpuList, error) {
 }
 
-func (ns *YakoNodeServer) GetSystemGpuInformation(ctx context.Context, empty *empty.Empty) (*yako.Gpu, error) {
+func (ns *YakoNodeServer) GetSystemGpuInformation(ctx context.Context, empty *empty.Empty) (*yako.GpuList, error) {
 }
 
 func (ns *YakoNodeServer) GetSystemMemoryInformation(ctx context.Context, empty *empty.Empty) (*yako.Memory, error) {

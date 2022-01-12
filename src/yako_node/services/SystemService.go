1c6e27dd9c52ff634a7c6768947661001eed22b8
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-12 21:40:05 +0000

    #23 feat: get system information rpc service implementation - Encode system information to gRPC struct

--- src/yako_node/services/SystemService.go
+++ src/yako_node/services/SystemService.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/golang/protobuf/ptypes/empty"
 	"yako/src/grpc/yako"
+	"yako/src/model"
 )
 
 // YakoNodeServer implements NodeServiceServer interface
@@ -11,6 +12,18 @@ type YakoNodeServer struct {
 }
 
 func (ns *YakoNodeServer) GetSystemInformation(ctx context.Context, empty *empty.Empty) (*yako.SysInfo, error) {
+	sysinfo := model.SysInfo{}
+	i := sysinfo.GetResources().(model.SysInfo)
+
+	info := &yako.SysInfo{
+		SysName:  i.SysName,
+		Machine:  i.Machine,
+		Version:  i.Version,
+		Release:  i.Release,
+		NodeName: i.NodeName,
+	}
+
+	return info, nil
 }
 
 func (ns *YakoNodeServer) GetSystemCpuInformation(ctx context.Context, empty *empty.Empty) (*yako.CpuList, error) {

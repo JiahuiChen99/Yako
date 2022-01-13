e838ec4dd675c2b13988d429b704b37e7aee4660
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-13 19:36:57 +0000

    feat: grpc client main

--- src/main.go
+++ src/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"yako/src/model"
+	"google.golang.org/grpc"
+	"log"
+	"yako/src/grpc/yako"
 )
 
 func main() {
-	cpu := model.Cpu{}
-	cpuList := cpu.GetResources()
-	gpu := model.Gpu{}
-	gpuList := gpu.GetResources()
+	cc, err := grpc.Dial("localhost:8000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln("Error al connectar")
+	}
+	defer cc.Close()
+
+	c := yako.NewNodeServiceClient(cc)
+
 }

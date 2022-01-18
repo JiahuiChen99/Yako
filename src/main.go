66f81582eb6253e6a450281872bdbdd5f4dc551a
JiahuiChen99 <Jiahui1@hotmail.es>
2022-01-18 18:36:22 +0000

    #18 feat: yako master gets info from yako agent

--- src/main.go
+++ src/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 	"log"
 	"yako/src/grpc/yako"
@@ -15,4 +18,22 @@ func main() {
 
 	c := yako.NewNodeServiceClient(cc)
 
+	var sysInfo *yako.SysInfo
+	var cpuInfo *yako.CpuList
+	var gpuInfo *yako.GpuList
+	var memInfo *yako.Memory
+
+	sysInfo, err = c.GetSystemInformation(context.Background(), &empty.Empty{})
+	cpuInfo, err = c.GetSystemCpuInformation(context.Background(), &empty.Empty{})
+	gpuInfo, err = c.GetSystemGpuInformation(context.Background(), &empty.Empty{})
+	memInfo, err = c.GetSystemMemoryInformation(context.Background(), &empty.Empty{})
+
+	if err != nil {
+		fmt.Println("Error")
+	}
+
+	fmt.Println(sysInfo)
+	fmt.Println(cpuInfo)
+	fmt.Println(gpuInfo)
+	fmt.Println(memInfo)
 }

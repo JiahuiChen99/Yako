9218a8ccb965102bf83aeeca78fe4436eb1e5996
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-07 17:38:19 +0000

    #38 feat: upload app get uploaded file

--- src/yako_master/API/Controller/deploy.go
+++ src/yako_master/API/Controller/deploy.go
@@ -3,11 +3,18 @@ package Controller
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"yako/src/yako_master/API/utils"
 )
 
 // UploadApp handles the file that the user wants
 // to deploy in the cluster
 func UploadApp(c *gin.Context) {
+	file, formErr := c.FormFile("app")
+	if formErr != nil {
+		err := utils.BadRequestError(formErr.Error())
+		c.JSON(err.Status, err)
+		return
+	}
 
 	// File uploaded and stored
 	c.JSON(http.StatusOK, map[string]string{"status": "uploaded successfully"})

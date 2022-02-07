d554158c644e674242601db576d925675566ab78
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-07 16:03:18 +0000

    #33 feat: start gin gonic function

--- src/main.go
+++ src/main.go
@@ -3,11 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 	"log"
 	"yako/src/grpc/yako"
 )
+
+func APIServer(wg *sync.WaitGroup) {
+	r := gin.Default()
+
+	// TODO: Use environment variables or secrets managers like Hashicorp Vault
+	err := r.Run(":8001")
+	if err != nil {
+		// TODO: Use logger
+		panic("API gin Server could not be started!")
+	}
+
+	// Tell wait group once the go routine is ended
+	wg.Done()
+}
 
 func main() {
 	cc, err := grpc.Dial("localhost:8000", grpc.WithInsecure())

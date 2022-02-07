8822bdf0813469028c88ef8e5a33312eb9699247
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-07 16:52:22 +0000

    #35 feat: add controller to alive endpoint

--- src/yako_master/API/routing.go
+++ src/yako_master/API/routing.go
@@ -1,11 +1,14 @@
 package API
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"yako/src/yako_master/API/Controller"
+)
 
 // AddRoutes specifies yakoAPI routes and its handlers
 func AddRoutes(router *gin.Engine) {
 	// handler for system heartbeat
-	router.GET("/alive", nil)
+	router.GET("/alive", Controller.IsAlive)
 	// handler for app deploying
 	router.GET("/deploy", nil)
 	// handler for cluster information

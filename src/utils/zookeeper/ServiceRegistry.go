faa5781dc250af43eb916fcb49e439438c14d4a2
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-11 21:24:07 +0000

    #40 feat: get all service addresses

--- src/utils/zookeeper/ServiceRegistry.go
+++ src/utils/zookeeper/ServiceRegistry.go
@@ -1,6 +1,7 @@
 package zookeeper
 
 import (
+	"fmt"
 	"github.com/go-zookeeper/zk"
 	"log"
 	"time"
@@ -53,4 +54,62 @@ func RegisterToCluster(zkp *zk.Conn, yakoNodeAddress string) string {
 	log.Printf("Registered to the Service Registry: %s", path)
 
 	return path
+}
+
+// updateServices is called whenever an event happens in zookeeper
+// it could be either a service disconnection or a new service registry
+func updateServices(zkp *zk.Conn, newService chan bool) {
+	fmt.Println("Updating cluster services list")
+	GetAllServiceAddresses(zkp, newService)
+}
+
+// GetAllServiceAddresses consults zookeeper service registry and
+// watches for any change
+func GetAllServiceAddresses(zkp *zk.Conn, newService chan bool) {
+	// Retrieve all znodes from service registry
+	yakoagents, _, watch, err := zkp.ChildrenW(RegistryZnode)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	var addresses []string
+
+	for _, service := range yakoagents {
+		yakoagentPath := fmt.Sprintf("%s/%s", RegistryZnode, service)
+		exists, _, err := zkp.Exists(yakoagentPath)
+		if err != nil {
+			log.Fatalln("Error while trying to check for " + yakoagentPath)
+		}
+
+		// Race condition, check if yakoagent exists
+		if !exists {
+			continue
+		}
+
+		// Get yakoagent socket
+		socket, _, err := zkp.Get(yakoagentPath)
+		if err != nil {
+			log.Fatalln("Error while trying to fetch data from " + yakoagentPath)
+		}
+
+		// Add the socket to the service registry list
+		addresses = append(addresses, string(socket[:]))
+	}
+
+	ServicesRegistry = addresses
+
+	// Log cluster available services
+	for i, yakoagent := range ServicesRegistry {
+		log.Println(fmt.Sprintf("[%d] %s", i, yakoagent))
+	}
+
+	select {
+	case event := <-watch:
+		if event.Type != zk.EventNodeChildrenChanged {
+			return
+		}
+		newService <- true
+		// TODO: Check if it's a new service registration & get the registered service socket
+		updateServices(zkp, newService)
+	}
 }

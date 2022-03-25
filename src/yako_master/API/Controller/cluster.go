424a99eef768df5e07629bdbf789e24b19221465
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-25 19:18:40 +0000

    #43 feat: compose response with master & service registries

--- src/yako_master/API/Controller/cluster.go
+++ src/yako_master/API/Controller/cluster.go
@@ -5,12 +5,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"yako/src/model"
 	"yako/src/utils/zookeeper"
 )
 
 // Cluster returns the cluster schema
 func Cluster(c *gin.Context) {
-	clusterSchema, err := json.Marshal(zookeeper.ServicesRegistry)
+	var response model.Response
+	// Get master & service registries and compose response
+	response.YakoMasters = zookeeper.MasterRegistry
+	response.YakoAgents = zookeeper.ServicesRegistry
+	clusterSchema, err := json.Marshal(response)
 	if err != nil {
 		log.Println(err)
 	}

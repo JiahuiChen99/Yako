e82ef526d5eb735fd690b99fc9433edc62a8aa22
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-23 17:14:15 +0100

    #34 feat: deploy application client logic

--- src/yako_master/API/Controller/deploy.go
+++ src/yako_master/API/Controller/deploy.go
@@ -2,12 +2,17 @@ package Controller
 
 import (
 	"container/heap"
+	"context"
+	"github.com/JiahuiChen99/Yako/src/grpc/yako"
 	"github.com/JiahuiChen99/Yako/src/model"
 	"github.com/JiahuiChen99/Yako/src/utils/directory_util"
 	"github.com/JiahuiChen99/Yako/src/utils/zookeeper"
 	"github.com/JiahuiChen99/Yako/src/yako_master/API/utils"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 // UploadApp handles the file that the user wants
@@ -107,4 +112,43 @@ func compliesWithMemory(agent *model.ServiceInfo, config model.Config, browniePo
 	if agent.Memory.Free >= config.SysMemory {
 		*browniePoints++
 	}
+}
+
+// deployApp opens application binary file, splices it up into chunks of 1KB
+// and sends it through gRPC streaming service
+func deployApp(c *yako.NodeServiceClient, appPath string) *yako.DeployStatus {
+	file, err := os.Open(appPath)
+	if err != nil {
+		log.Println("Could not open the file")
+		return nil
+	}
+
+	stream, err := (*c).DeployAppToAgent(context.Background())
+
+	// 1KB buffer
+	buf := make([]byte, 1024)
+
+	// Start transmission
+	transmitting := true
+	nBytes := 0
+	for transmitting {
+		nBytes, err = file.Read(buf)
+
+		// End of File
+		if err == io.EOF {
+			transmitting = false
+		}
+
+		err = stream.Send(&yako.Chunk{
+			Content: buf[:nBytes],
+		})
+		if err != nil {
+			log.Println("Error while deploying the application")
+		}
+	}
+
+	var deployStatus *yako.DeployStatus
+	deployStatus, err = stream.CloseAndRecv()
+
+	return deployStatus
 }

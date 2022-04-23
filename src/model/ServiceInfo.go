af58cb0c5320c827dcdd405f605aecc2b02ae201
JiahuiChen99 <Jiahui1@hotmail.es>
2022-04-23 16:42:20 +0100

    #34 feat: agent struct for the service registry

--- src/model/ServiceInfo.go
+++ src/model/ServiceInfo.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/JiahuiChen99/Yako/src/grpc/yako"
+
 // ServiceInfo struct that represents the data transferred back to
 // the client side
 type ServiceInfo struct {
@@ -15,4 +17,10 @@ type ServiceInfo struct {
 type Response struct {
 	YakoMasters map[string]*ServiceInfo `json:"yako_masters"`
 	YakoAgents  map[string]*ServiceInfo `json:"yako_agents"`
+}
+
+// Agent struct is used in the Service Registry
+type Agent struct {
+	ServiceInfo *ServiceInfo
+	GrpcClient  yako.NodeServiceClient
 }

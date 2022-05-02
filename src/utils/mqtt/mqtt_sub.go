0cda27dedd49974307d1039f403f64582e523f62
JiahuiChen99 <Jiahui1@hotmail.es>
2022-05-02 17:38:38 +0100

    #60 feat: mqtt topic constants

--- src/utils/mqtt/mqtt_sub.go
+++ src/utils/mqtt/mqtt_sub.go
@@ -5,6 +5,17 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+const (
+	TopicCpu     = "topic/cpu"
+	TopicGpu     = "topic/gpu"
+	TopicMemory  = "topic/memory"
+	TopicSysInfo = "topic/sysinfo"
+)
+
+var (
+	topics = []string{TopicCpu, TopicGpu, TopicMemory, TopicSysInfo}
+)
+
 // ConnectMqttBroker connects to an MQTT Broker and returns the connection
 // YakoMaster to listen for subscribed channels
 func ConnectMqttBroker(mqttBrokerIp string, mqttBrokerPort string) {

b28d26f5efa59672beb9efc2c5f1d8ed3115af19
JiahuiChen99 <Jiahui1@hotmail.es>
2022-05-02 17:39:16 +0100

    #60 feat: mqtt handlers - message - on connection - on connection lost

--- src/utils/mqtt/mqtt_sub.go
+++ src/utils/mqtt/mqtt_sub.go
@@ -3,6 +3,7 @@ package mqtt
 import (
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
 )
 
 const (
@@ -23,9 +24,43 @@ func ConnectMqttBroker(mqttBrokerIp string, mqttBrokerPort string) {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(fmt.Sprintf("%s:%s", mqttBrokerIp, mqttBrokerPort))
 
+	opts.SetDefaultPublishHandler(messageHandler)
+	opts.SetOnConnectHandler(connectionHandler)
+	opts.SetConnectionLostHandler(connectionLostHandler)
+
 	// Connect to broker and obtain a new connection
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		fmt.Println("Error while connecting to the MQTT broker. MQTT service unavailable, please restart the service")
+	}
+
+}
+
+// messageHandler Callback handler for subscribed events. Processes the event
+// according to the message topic
+func messageHandler(client mqtt.Client, msg mqtt.Message) {
+	switch msg.Topic() {
+	case TopicCpu:
+		fmt.Println("CPU topic")
+	case TopicGpu:
+		fmt.Println("GPU topic")
+	case TopicMemory:
+		fmt.Println("Memory topic")
+	case TopicSysInfo:
+		fmt.Println("Sysinfo topic")
+	}
+}
+
+// connectionHandler on connection to MQTT broker handler
+func connectionHandler(client mqtt.Client) {
+	if client.IsConnected() {
+		log.Println("Connected to MQTT broker")
+	}
+}
+
+// connectionLostHandler on connection lost to MQTT broker handler
+func connectionLostHandler(client mqtt.Client, err error) {
+	if !client.IsConnected() {
+		log.Printf("Connection lost: %v\n", err)
 	}
 }

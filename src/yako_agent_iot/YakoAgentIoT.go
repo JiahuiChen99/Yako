2d015ce33246836d08183a6d82a2a3dca84b93bc
JiahuiChen99 <Jiahui1@hotmail.es>
2022-05-05 15:22:58 +0100

    #61 feat: iot yakoagent timed reports to the mqtt broker

--- src/yako_agent_iot/YakoAgentIoT.go
+++ src/yako_agent_iot/YakoAgentIoT.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/JiahuiChen99/Yako/src/model"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
 	"os"
+	"time"
 )
 
 const (
@@ -39,4 +43,61 @@ func main() {
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		fmt.Println("Error while connecting to the MQTT broker.")
 	}
+
+	// Regular reports to the MQTT broker
+	timedReport(client)
+}
+
+// timedReport schedules a timed system resources information report to the MQTT broker
+func timedReport(client mqtt.Client) {
+	var topic string
+	for {
+		log.Println("Reporting data to the broker")
+		for _, mqttTopic := range topics {
+			var data interface{}
+			var err error
+			switch mqttTopic {
+			case CPU:
+				cpu := model.Cpu{}
+				data, err = cpu.GetResources()
+				if err != nil {
+					log.Println("Error retrieving system's CPU data")
+				}
+				topic = fmt.Sprintf("/topic/%s/%s", AgentSocket, CPU)
+			case GPU:
+				data, err = model.Gpu{}.GetResources()
+				if err != nil {
+					log.Println("Error retrieving system's GPU data")
+				}
+				topic = fmt.Sprintf("/topic/%s/%s", AgentSocket, GPU)
+			case Memory:
+				data, err = model.Memory{}.GetResources()
+				if err != nil {
+					log.Println("Error retrieving system's memory data")
+				}
+				topic = fmt.Sprintf("/topic/%s/%s", AgentSocket, Memory)
+			case SysInfo:
+				data, err = model.SysInfo{}.GetResources()
+				if err != nil {
+					log.Println("Error retrieving system's information data")
+				}
+				topic = fmt.Sprintf("/topic/%s/%s", AgentSocket, SysInfo)
+			}
+			// Publish the data
+			pubTopic(client, topic, data)
+		}
+		// Sleep for 10 seconds
+		time.Sleep(10 * time.Second)
+	}
+}
+
+// pubTopic encodes and publish a message with topic to the MQTT broker server
+func pubTopic(client mqtt.Client, topic string, data interface{}) {
+	// Encode data to json format
+	message, err := json.Marshal(data)
+	if err != nil {
+		log.Print("Error while trying to encode the data", err)
+	}
+	token := client.Publish(topic, 0, true, message)
+	token.Wait()
 }
